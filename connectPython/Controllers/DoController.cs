81a8c121a5a0781b4b4509380d762c1ecb5a570d
tamarmiller1 <tamarm@darkeychana.com>
2022-12-02 10:47:24 +0200

    my commit message

---
+++ connectPython/Controllers/DoController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace connectPython.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DoController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<int> DoSomething(int x)
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    HttpResponseMessage response = await client.GetAsync($"http://127.0.0.1:5000/do?x={x}");
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        Console.Write("Success");
+                    }
+                    else
+                    {
+                        Console.Write("Failure");
+                    }
+
+                    return int.Parse(await response.Content.ReadAsStringAsync());
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+    }
+}
